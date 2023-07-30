54779f876c8036923fecb2781a819a6ac6a01ef9
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-07-30 16:58:16 +0300

    Added validation for ms array constructor method arguments. Updated unit tests to verify validation works.

--- src/msarray.py
+++ src/msarray.py
@@ -14,20 +14,18 @@ class MSArray(BaseModel):
   array: ndarray
         
   @staticmethod
+  @validate_call
   def new_shaped(item_count: int, axis_count: int, elements_per_axis_count: int):
     return MSArray(**{
       'array': np.arange(item_count).reshape(axis_count, elements_per_axis_count)
     })
   
   @staticmethod
-  def new_from_list(list: list):
+  @validate_call
+  def new_from_list(list: list[int]):
     return MSArray(**{
       'array': np.array(list)
     })
-  
-  @validate_call
-  def accept_int(self, number: int):
-    return int
 
 def test_new_shaped():
   array = MSArray.new_shaped(15,3,5).array
@@ -40,14 +38,15 @@ def test_new_shaped():
 def test_new_from_list():
   array = MSArray.new_from_list([1,2,3]).array
   assert(array.dtype.name)  == 'int64'
-  array = MSArray.new_from_list([1,2,3.]).array
-  assert(array.dtype.name)  == 'float64'
-
-def test_accept_int():
-  msarray = MSArray.new_from_list([1,2,3])
-  msarray.accept_int(3)
   try:
-    msarray.accept_int(3.1)
+    array = MSArray.new_from_list([1,2,3.1]).array
+  except:
+    return
+  raise Exception('Float value was incorrectly accepted in list argument')
+
+def test_validation():
+  try:
+    array = MSArray.new_shaped(15.1,3,5).array
   except:
     return
   raise Exception('Float value was incorrectly accepted')
