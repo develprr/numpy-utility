f07f20765de2c96130f9bc3fec8df9b35784d86b
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-08-03 05:48:07 +0300

    Created constructors for two-dimensonal arrays. Wrote unit tests.

--- src/msarray.py
+++ src/msarray.py
@@ -22,7 +22,7 @@ class MSArray(BaseModel):
   
   @staticmethod
   @validate_call
-  def new_from_int_list(list: list[int]):
+  def new_int_based_from_list(list: list[int]):
     return MSArray(**{
       'array': np.array(list)
     })
@@ -35,12 +35,26 @@ class MSArray(BaseModel):
     })
     
   @staticmethod
-  @validate_call(config=dict(arbitrary_types_allowed=True))
+  @validate_call
   def new_zero_dimensional(value: int):
     return MSArray(**{
       'array': np.array(value)
     })  
 
+  @staticmethod
+  @validate_call
+  def new_two_dimensional(list1: list, list2: list):
+    return MSArray(**{
+      'array': np.array([list1, list2])
+    })
+   
+  @staticmethod
+  @validate_call(config=dict(arbitrary_types_allowed=True))
+  def new_float_based_two_dimensional(list1: list[float], list2: list[float]):
+    return MSArray(**{
+      'array': np.array([list1, list2])
+    })
+    
 def test_new_shaped():
   array = MSArray.new_shaped(15,3,5).array
   assert(type(array)) == ndarray 
@@ -49,33 +63,66 @@ def test_new_shaped():
   assert(array.dtype.name) == 'int64'
   assert(array.ndim) == 2
 
-def test_new_from_int_list__works_when_list_of_integers_is_given():
-  array = MSArray.new_from_int_list([1, 2, 3]).array
+def test_new_int_based_from_list__pass_when_list_of_integers_is_given():
+  array = MSArray.new_int_based_from_list([1, 2, 3]).array
   assert(array.dtype.name)  == 'int64'
 
-def test_new_from_int_list__fails_when_not_all_values_are_integers():
+def test_new_int_based_from_list__fail_when_not_all_values_are_integers():
   try:
-    array = MSArray.new_from_int_list([1, 2, 3.1]).array
+    array = MSArray.new_int_based_from_list([1, 2, 3.1]).array
   except:
-    print("new_from_int_list: an exception was raised as expected!")
+    print("new_int_based_from_list: an exception was raised as expected!")
     return
   raise Exception('test fail: float value was incorrectly accepted in list argument')
 
-def test_new_from_ndarray__works_when_ndarray_is_given():
+def test_new_from_ndarray__pass_when_ndarray_is_given():
   array = MSArray.new_from_ndarray(np.array([1,2,3])).array
   assert(array.dtype.name)  == 'int64'
 
-def test_new_from_ndarray__fails_when_list_is_given():
+def test_new_from_ndarray__fail_when_list_is_given():
   try:
     array = MSArray.new_from_ndarray([1,2,3]).array
   except:
     print("new_from_ndarray: an exception was raised as expected!")
     return
-  raise Exception('test fail: constructor did not raise expection on wrong input parameter type')
+  raise Exception('test fail: constructor did not raise exception on wrong input parameter type')
 
 def test_new_zero_dimensional():
   array = MSArray.new_zero_dimensional(42).array
   assert(array == 42)
+  
+def test_new_two_dimensional__pass_when_two_lists_are_given():
+  array = MSArray.new_two_dimensional([1,2,3], [4,5,6])
+  
+def test_new_two_dimensional__pass_when_any_kinds_of_lists_are_given():
+  array = MSArray.new_two_dimensional([1, 2, 3.8], [4, "viis", 6.3]).array
+  assert(array.dtype.name) == "str1024"
+  
+def test_new_two_dimensional__fail_when_other_than_two_lists_are_given():
+  try: 
+    array = MSArray.new_two_dimensional([1, 2, 3.8], 12).array
+  except:
+    print("new_two_dimensional: an exception was raised as expected!")
+    return  
+  raise Exception("test fail: constructor did not raise exception when other than two lists were given")
+    
+def test_new_float_based_two_dimensional__pass_when_two_float_lists_are_given():
+  array = MSArray.new_float_based_two_dimensional([1.1, 2, 3], [4, 5, 6.3]).array
+  assert(array.dtype.name) == "float64"
+
+# Surprisingly, this must pass because int list is also a float list although a float list is not an int list.
+# Just like every dog is an animal but not every animal is a dog :) 
+def test_new_float_based_two_dimensional__pass_when_two_int_lists_are_given():
+  array = MSArray.new_float_based_two_dimensional([1, 2, 3], [4, 5, 6]).array
+  assert(array.dtype.name) == "float64"
+
+def test_new_float_based_two_dimensional__fail_when_arbitrary_lists_are_given():
+  try:
+    array = MSArray.new_float_based_two_dimensional([1, 2, 3], ["neljä", 5, 6]).array
+  except:
+    print("pass: exception was raised as expected")
+    return
+  raise Exception("fail: exception was not raised as expected")
   
 def test_validation():
   try:
