11bc5d4ff6d0b5acbdaa9d7f665f7260057f0d98
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-08-05 08:13:20 +0300

    Updated D2FloatArray constructor

--- src/d2floatarray.py
+++ src/d2floatarray.py
@@ -15,19 +15,19 @@ class D2FloatArray(BaseModel):
 
   @staticmethod
   @validate_call
-  def new_from_lists(*lists: list[float]):
+  def new(array: list[list[float]]):
     return D2FloatArray(**{
-      'ndarray': np.array(lists)
+      'ndarray': np.array(array)
     })
 
 # Perhaps surprisingly, this must pass because an int list is also a float list although a float list is not an int list.
-def test_new_from_lists__pass_when_two_int_lists_are_given():
-  array = D2FloatArray.new_from_lists([1, 2, 3], [4, 5, 6], [7, 8, 9]).ndarray
+def test_new__pass_when_three_int_lists_are_given():
+  array = D2FloatArray.new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).ndarray
   assert(array.dtype.name) == "float64"
 
-def test_new_from_lists__fail_when_arbitrary_lists_are_given():
+def test_new__fail_when_arbitrary_lists_are_given():
   try:
-    array = D2FloatArray.new_from_lists([1, 2, 3], ["neljä", 5, 6]).ndarray
+    array = D2FloatArray.new([1, 2, 3], ["neljä", 5, 6]).ndarray
   except:
     print("pass: exception was raised as expected")
     return
